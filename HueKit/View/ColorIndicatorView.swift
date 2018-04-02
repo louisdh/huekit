77bcefa65c10814c82be47c228c72100947814e5
Louis D'hauwe <louisdhauwe@silverfox.be>
2018-04-02 16:52:36 +0200

    Implement initalizer

--- HueKit/View/ColorIndicatorView.swift
+++ HueKit/View/ColorIndicatorView.swift
@@ -31,7 +31,8 @@ public class ColorIndicatorView: UIView {
 	}
 	
 	required public init?(coder aDecoder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
+		super.init(coder: aDecoder)
+
 	}
 		
 	override public func draw(_ rect: CGRect) {
